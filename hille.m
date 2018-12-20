d43eb4c78480d98559e00ddd9b32f4229e14dc6c
= <alec.hoyland@gmail.com>
2018-12-20 11:05:29 -0500

    Add stylesheet and test code

--- hille.m
+++ hille.m
@@ -1,1 +1,19 @@
+%% Hille Series Trajectory Tracing
+% Alec Hoyland
+% 2018-12-20 10:59
 
+pHeader;
+tic
+
+%% Introduction
+% The Hille series is equivalent to a discretized Taylor series under the limit
+% $$ \lim_{\Delta t \rightarrow 0} \sum_{n=0}^{\infty} \frac{t^n}{n! \Delta t^n} \mathrm{D}^n f(a) = f(a + t)$$ for $t > 0$.
+
+%% Version Info
+pFooter;
+
+t = toc;
+
+%%
+% This document was built in:
+disp(strcat(oval(t,3),' seconds.'))
