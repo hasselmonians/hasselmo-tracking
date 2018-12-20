2c034c133cd671d472994a759fc371ed3f51b8b6
= <alec.hoyland@gmail.com>
2018-12-20 11:06:55 -0500

    Update gitignore

--- hille.m
+++ hille.m
@@ -7,7 +7,7 @@ tic
 
 %% Introduction
 % The Hille series is equivalent to a discretized Taylor series under the limit
-% $$ \lim_{\Delta t \rightarrow 0} \sum_{n=0}^{\infty} \frac{t^n}{n! \Delta t^n} \mathrm{D}^n f(a) = f(a + t)$$ for $t > 0$.
+% $$ \lim_{\Delta t \rightarrow 0} \sum_{n=0}^{\infty} \frac{t^n}{n! \Delta t^n} \mathrm{D}^n f(a) = f(a + t) $$ for $t > 0$.
 
 %% Version Info
 pFooter;
